38d961824f15fb6a5f12e804775db7f967221b97
Israr <abu.israr.khan.9@gmail.com>
2021-06-11 23:16:10 +0530

    sideChatUI

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,13 @@
+
+import React from 'react'
 import { Avatar, IconButton} from '@material-ui/core'
-import { MoreVertIcon} from '@material-ui/icons/MoreVert'
+import  MoreVertIcon from '@material-ui/icons/MoreVert'
 import DonutLargeIcon from '@material-ui/icons/DonutLarge'
 import ChatIcon from '@material-ui/icons/Chat'
-import React from 'react'
+
 import './Sidebar.css'
+import { SearchOutlined } from '@material-ui/icons'
+import SidebarChat from './SidebarChat'
 
 function Sidebar() {
     return (
@@ -16,11 +20,23 @@ function Sidebar() {
 
                 
                 <div className ="sidebar__headerRight">
-                    <IconButton><DonutLargeIcon/></IconButton>
-                    <IconButton><ChatIcon/></IconButton>
-                    <IconButton><MoreVertIcon/></IconButton>
+                    <IconButton ><DonutLargeIcon clasName="icon"/></IconButton>
+                    <IconButton><ChatIcon clasName="icon"/></IconButton>
+                    <IconButton><MoreVertIcon clasName="icon"/></IconButton>
 
                 </div>
+            </div>
+            <div className="sidebar__search">
+                <div className = "sidebar__searchContainer">
+                    <SearchOutlined/>
+                    <input placeholder="Search or start new chat" type="text"/>
+                </div>
+            </div>
+            <div className="sidebar__chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
             </div>
             
         </div>
